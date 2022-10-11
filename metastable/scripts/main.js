d9539530a70f5f799b724602848e27c55190c746
Rachel Dixon <ms.racheldixon@gmail.com>
2022-10-10 20:01:46 -0400

    desperation index switching

--- metastable/scripts/main.js
+++ metastable/scripts/main.js
@@ -3,3 +3,26 @@ document.querySelector('body').appendChild(newdiv);
 console.log('HI IM WORKING');
 
 const obj = { name: 'omar' };
+const words1 = ['theorization', 'at best', 'resistance', 'ability', 'recognize', 'cultivate', 'exploration']
+
+/*do {
+    return words1
+    while:HTMLDivElement=
+}
+*/
+
+let view = document.getElementById('images-next');
+let close = document.getElementById('images-static');
+
+let open = function() {
+    images-next.style.display = 'block';
+    images-static.style.display = 'block';
+}
+
+let hide = function() {
+    images-next.style.display = 'none';
+    images-static.style.display = 'none';
+}
+
+view.addEventListener('click', open);
+close.addEventListener('click', hide);
