c1672aaf7bb5fc3b02276fada46d99bde889be67
Rachel Dixon <ms.racheldixon@gmail.com>
2022-10-18 12:39:58 -0400

    infuriating link bug fix

--- p5 template/scripts/main.js
+++ p5 template/scripts/main.js
@@ -4,15 +4,27 @@ function setup() {
     //this function runs once when the webpage is loaded
     //within this function, you can use p5
     createCanvas(640, 360);
-    background(255);
 }
 
 function draw() {
     //this functions runs many times every second!
     //within this function, you can use p5 syntax
-    rect(100, 200, 75, 150);
-    bezier()
+    background(255);
+    ellipseMode(CENTER);
+    rectMode(CENTER);
+
+//Body
+    rect(240,145,20,100);
+//head
+    ellipse(255,115,60,60)
+//eyes
+    ellipse(221,115,16,32);
+    ellipse(259,115,16,32);
 }
+
+
+
+
 
 
 
