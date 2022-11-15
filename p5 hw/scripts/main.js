bf36948e0e02cff0fec7f58db2245ba07cce8906
Rachel Dixon <ms.racheldixon@gmail.com>
2022-11-15 14:39:07 -0500

    js5 blobby exercise?

--- p5 hw/scripts/main.js
+++ p5 hw/scripts/main.js
@@ -1,18 +1,38 @@
+// var blob
+
+// var blobs = []
+// var zoom = 1;
+
 function setup() {
-    createCanvas(720, 400);
-    background(230);
-    strokeWeight(2);
+    createCanvas(600, 600);
+    background(255)
+    blob = new Blob(0, 0, 128);
   }
-  
+
   function draw() {
-    if (mouseIsPressed) {
-      stroke(255);
-    } else {
-      stroke(237, 34, 93);
+    background(1);
+    strokeWeight(2);
+
+     this.constrain = function(){
+       blob.pos.x = constrain(blob.pos.x, -width / 4, width/4);
+       blob.pos.y = constrain(blob.pos.y, -heaight / 4, height/4);
+  }
+
+    this.show = function(){
+      fill(255);
+      ellipse(this.pos.x, this.pos.y, this.r * 2, this.r *2);
     }
-    line(mouseX - 66, mouseY, mouseX + 66, mouseY);
-    line(mouseX, mouseY - 66, mouseX, mouseY + 66);
-   
+    push();
+    translate(this.pos.x, this.pos.y);
+    beginShape();
+
+    for (var a=0; a < TWO_PI; a+= +0.1){
+      var x = this.r * cos(a);
+      var y = this.r * sin(a);
+      vertex(x,y);
+    }
+    endShape();
+    pop();
   }
 // function setup(){
 //     createCanvas(400,400);
@@ -25,7 +45,6 @@ function setup() {
 //     fill('red');
 //     circle(width/2, height*.1, 100);
 //}
-
 //newdiv = document.createElement('div');
 //document.querySelector('body').appendChild(newdiv);
 //console.log('HI IM WORKING');
