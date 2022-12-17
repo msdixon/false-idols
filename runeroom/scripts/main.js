ffa9eaa95cb97a048f6e2959bd90d4e99dedb11f
Rachel Dixon <ms.racheldixon@gmail.com>
2022-12-17 16:01:01 -0500

    fixed main container added name

--- runeroom/scripts/main.js
+++ runeroom/scripts/main.js
@@ -4,12 +4,32 @@
 console.log(5 + 6);
 
 var openButton = document.getElementsByClassName("open");
-var onButtonClick = function() {
-    openButton.textContent = "sup";
-};
+
+// var onButtonClick = function() {
+//     openButton.textContent = "sup";
+// };
 
 MicroModal.init({
     disableScroll: true
   });
     
 console.log("yo");
+
+var backgroundImages = ["/runeroom/assets/img/bg1.jpg/", "runeroom/assets/img/bg2.jpg", "runeroom/assets/img/bg3.jpg", "runeroom/assets/img/bg4.jpg", "runeroom/assets/img/bg5.jpg", "runeroom/assets/img/bg6.jpg", "runeroom/assets/img/bg7.jpg", "runeroom/assets/img/bg9.jpg", "runeroom/assets/img/bg10.jpg"];
+// var i = 0, i > backgroundImages.length, i++ ={}
+document.querySelector("openButton")
+document.addEventListener("click", function(e) {
+  for (let i=0; i < backgroundImages.length; i++);{
+  document.querySelector("cluster").style.background = backgroundImages[i];}
+});
+
+console.log("you tried it");
+
+
+// var onButtonClick = function() {
+//   random_bg.innerHTML
+// }
+
+// buttonEl.addEventListener("click",)(function(e){
+//   document.querySelector("background-image").onclick = function() {get; };
+// }());
