09b660fd5d5a274a41991b1238b0e47ddce9c8c1
Rachel Dixon <ms.racheldixon@gmail.com>
2022-12-18 15:25:45 -0500

    many horrible changes for background

--- runeroom/scripts/main.js
+++ runeroom/scripts/main.js
@@ -3,33 +3,24 @@
 // })
 console.log(5 + 6);
 
-var openButton = document.getElementsByClassName("open");
-
-// var onButtonClick = function() {
-//     openButton.textContent = "sup";
-// };
-
 MicroModal.init({
     disableScroll: true
   });
     
 console.log("yo");
+// find element for event to be on
+var openButton = document.getElementsByClassName("open");
+var clusterEl = document.getElementByClassName("cluster");
 
-var backgroundImages = ["/runeroom/assets/img/bg1.jpg/", "runeroom/assets/img/bg2.jpg", "runeroom/assets/img/bg3.jpg", "runeroom/assets/img/bg4.jpg", "runeroom/assets/img/bg5.jpg", "runeroom/assets/img/bg6.jpg", "runeroom/assets/img/bg7.jpg", "runeroom/assets/img/bg9.jpg", "runeroom/assets/img/bg10.jpg"];
-// var i = 0, i > backgroundImages.length, i++ ={}
-document.querySelector("openButton")
-document.addEventListener("click", function(e) {
-  for (let i=0; i < backgroundImages.length; i++);{
-  document.querySelector("cluster").style.background = backgroundImages[i];}
-});
+var document.querySelectorAll("cluster")
 
-console.log("you tried it");
-
-
-// var onButtonClick = function() {
-//   random_bg.innerHTML
+// for (var child of myElement.children) {
+//   console.log(child.tagName);
 // }
-
-// buttonEl.addEventListener("click",)(function(e){
-//   document.querySelector("background-image").onclick = function() {get; };
-// }());
+// define event listener function
+var onButtonClick = function(e) {
+  var backgroundImages = ["/runeroom/assets/img/bg1.jpg/", "runeroom/assets/img/bg2.jpg", "runeroom/assets/img/bg3.jpg", "runeroom/assets/img/bg4.jpg", "runeroom/assets/img/bg5.jpg", "runeroom/assets/img/bg6.jpg", "runeroom/assets/img/bg7.jpg", "runeroom/assets/img/bg9.jpg", "runeroom/assets/img/bg10.jpg"];
+  var changeClusterEl = document.addEventListener("click", function() {
+  for (let i=0; i < backgroundImages.length; i++);{
+    = backgroundImages[i];}
+}
