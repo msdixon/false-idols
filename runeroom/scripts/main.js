f8dd27c80cddde890423f9ed8773480091f78416
Rachel Dixon <ms.racheldixon@gmail.com>
2022-12-18 18:42:11 -0500

    the runes are looking good let's ship it

--- runeroom/scripts/main.js
+++ runeroom/scripts/main.js
@@ -9,18 +9,29 @@ MicroModal.init({
     
 console.log("yo");
 // find element for event to be on
-var openButton = document.getElementsByClassName("open");
-var clusterEl = document.getElementByClassName("cluster");
+var cardsBackground = document.querySelector(".cards");
+var cards = document.querySelectorAll(".card");
+var backgroundImages = ["/runeroom/assets/img/bg1.jpg/", "/runeroom/assets/img/bg2.jpg", "/runeroom/assets/img/bg3.jpg", "/runeroom/assets/img/bg4.jpg", "/runeroom/assets/img/bg5.jpg", "/runeroom/assets/img/bg8.jpg", "/runeroom/assets/img/bg6.jpg", "/runeroom/assets/img/bg7.jpg", "/runeroom/assets/img/bg9.jpg", "/runeroom/assets/img/bg10.jpg"];
+cards.forEach(card => {
+  card.addEventListener('click', function() {
+    var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+    cardsBackground.style.backgroundImage = `url(${randomImage})`;
+    
+  console.log("6+9") ;
+  });
+});
 
-var document.querySelectorAll("cluster")
+// var button = document.querySelector(".cards");
+// button.addEventListener =  ("click", function(){
+
+
+// })
 
 // for (var child of myElement.children) {
 //   console.log(child.tagName);
 // }
 // define event listener function
-var onButtonClick = function(e) {
-  var backgroundImages = ["/runeroom/assets/img/bg1.jpg/", "runeroom/assets/img/bg2.jpg", "runeroom/assets/img/bg3.jpg", "runeroom/assets/img/bg4.jpg", "runeroom/assets/img/bg5.jpg", "runeroom/assets/img/bg6.jpg", "runeroom/assets/img/bg7.jpg", "runeroom/assets/img/bg9.jpg", "runeroom/assets/img/bg10.jpg"];
-  var changeClusterEl = document.addEventListener("click", function() {
-  for (let i=0; i < backgroundImages.length; i++);{
-    = backgroundImages[i];}
-}
+// var onButtonClick = function(e) {
+//  document.addEventListener("click", function() {
+//     = backgroundImages[i];}
+// }
